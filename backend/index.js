5ddbb6e11d3c741514801cc963c9a6fd8d438422
HauserS22081 <HauserS22081@sus.htl-grieskirchen.at>
2025-12-11 12:15:51 +0100

    add -A

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@
   app.use(cors());
   app.use(express.json());
 
-  const AVG_BACKEND = process.env.PORT || "http://10.230.16.39:50587";
+  const AVG_BACKEND = process.env.AVG_BACKEND || "http://10.230.18.52:50587";
 
 
   const tasks = {}; // taskId -> { status, itemsToPick, result }
@@ -43,7 +43,7 @@
     console.log("Task created:", taskId);
 
     if (!currentTaskId) {
-      // startTask(newTask);
+      startTask(newTask);
     } else {
       taskQueue.push(newTask);
       console.log("Task queued:", taskId);
@@ -93,7 +93,7 @@
 
   const interval = setInterval(async () => {
     try {
-      const resp = await axios.get(`${AVG_BACKEND}/status`);
+      const resp = await axios.get(`${AVG_BACKEND}/status`, { params: { taskId } });
       if (!resp.data || !resp.data.status) return;
 
       const pyStatus = resp.data.status;
